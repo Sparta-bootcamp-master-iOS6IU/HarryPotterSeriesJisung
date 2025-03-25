df23af68f71c4ac7209c2b08423ff0b432955f14
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-25 19:36:18 +0900

    🔧 Config: 프로젝트 초기 설정
    - Main.storyboard 관련 제거
    - SceneDelegate.swift 수정
    - SnapKit 추가
    - SwiftLint 추가
    - SwiftLint 설정에 맞게 수정
    - .gitignore 추가
    - Assets에 이미지 파일 추가
    - data.json 파일 추가

--- HarryPotterSeries/ViewController.swift
+++ HarryPotterSeries/ViewController.swift
@@ -1,19 +1,4 @@
-//
-//  ViewController.swift
-//  HarryPotterSeries
-//
-//  Created by 박지성 on 3/25/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
