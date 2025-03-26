6dfba9d0689306960d86c8d98995fcf6c28d4874
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-26 16:48:35 +0900

    💡 Comment: 문서화 주석 추가

--- HarryPotterSeries/Service/BookAPIService.swift
+++ HarryPotterSeries/Service/BookAPIService.swift
@@ -1,9 +1,10 @@
 import Foundation
-import os
 
 struct BookAPIService {
     static let shared = BookAPIService()
 
+    /// JSON 파일에서 책 목록을 가져오는 메서드
+    /// - Returns: Book 배열 또는 오류 시 `nil` 반환
     func fetchBooks() -> [Book]? {
         guard let path = Bundle.main.path(forResource: DataFile.name, ofType: DataFile.type) else {
             AppLogger.api.error("\(Logging.Message.invaliedPath)")
