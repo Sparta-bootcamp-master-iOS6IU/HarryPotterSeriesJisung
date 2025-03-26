0ffadbfed7f43b966f173a4969712e65dfba18d0
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-26 14:15:46 +0900

    ✨ Feat: data.json 디코딩 기능 구현
    모델 정의
    - Book → attributes 객체 속성
    - BookAPIResponse → data
    - BookData → attributes
    - Chapter → chapter

---
+++ HarryPotterSeries/Service/BookAPIService.swift
@@ -0,0 +1,28 @@
+import Foundation
+import os
+
+struct BookAPIService {
+    static let shared = BookAPIService()
+
+    func fetchBooks() -> [Book]? {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+            return nil
+        }
+
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let decodedData = try JSONDecoder().decode(BookAPIResponse.self, from: data)
+
+            let books = decodedData.data.enumerated().map { index, bookData -> Book in
+                var book = bookData.attributes
+                book.order = "\(index + 1)"
+
+                return book
+            }
+
+            return books
+        } catch {
+            return nil
+        }
+    }
+}
