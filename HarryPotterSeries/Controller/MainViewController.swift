cbbeb30d0d33ba0f18940163c43732b9fa4c6297
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-26 16:37:57 +0900

    🎨 UI: 시리즈 순서 버튼 구현

--- HarryPotterSeries/Controller/MainViewController.swift
+++ HarryPotterSeries/Controller/MainViewController.swift
@@ -5,6 +5,7 @@ class MainViewController: UIViewController {
     var books: [Book] = []
 
     let bookTitleLabel = UILabel()
+    let seriesOrderButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +25,24 @@ class MainViewController: UIViewController {
         bookTitleLabel.textAlignment = .center
         bookTitleLabel.numberOfLines = 2
 
-        view.addSubview(bookTitleLabel)
+        seriesOrderButton.setTitle(books[0].order, for: .normal)
+        seriesOrderButton.titleLabel?.textAlignment = .center
+        seriesOrderButton.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(Layout.FontSize.small))
+        seriesOrderButton.layer.cornerRadius = CGFloat(Layout.SeriesOrderButton.radius)
+        seriesOrderButton.backgroundColor = .systemBlue
+
+        [bookTitleLabel, seriesOrderButton]
+            .forEach { view.addSubview($0) }
 
         bookTitleLabel.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(Layout.Constraints.insetStandard)
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
-                .offset(Layout.Constraints.spacingBetweenTitleAndSeriesOrder)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(Layout.Constraints.insetTopSafeArea)
+        }
+
+        seriesOrderButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(bookTitleLabel.snp.bottom).offset(Layout.Constraints.spacingBetweenTitleAndSeriesOrder)
+            $0.width.height.equalTo(Layout.SeriesOrderButton.size)
         }
     }
 }
--- HarryPotterSeries/Util/Constant/Layout.swift
+++ HarryPotterSeries/Util/Constant/Layout.swift
@@ -1,10 +1,17 @@
 enum Layout {
     enum FontSize {
         static let large = 24
+        static let small = 16
     }
 
     enum Constraints {
         static let insetStandard = 20
-        static let spacingBetweenTitleAndSeriesOrder = 10
+        static let spacingBetweenTitleAndSeriesOrder = 16
+        static let insetTopSafeArea = 10
+    }
+
+    enum SeriesOrderButton {
+        static let size = 40
+        static let radius = 20
     }
 }
